422dd800b1a82b4a2a927b4e9e1ee18f528c4b33
Marcos653 <marcos.costas@xbrain.com.br>
2024-03-19 08:46:06 -0300

    test: add failing test for productsServiceImpl getAll

---
+++ src/test/java/com/example/productsapi/domain/service/ProductServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.example.productsapi.domain.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Fail.fail;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceImplTest {
+
+    @Test
+    void getAll_ShouldReturnListProducts_whenCalled() {
+        fail("Not implemented");
+    }
+}
